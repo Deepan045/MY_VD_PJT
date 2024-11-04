0526be42fe4fe4d7418a0f6de1e7fcfd8f015eb4
Deepan M <deepanmurugan0225@gmail.com>
2024-11-04 15:04:05 +0530

    Created Footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
-
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
   <>
     <Header></Header>
+    <Container>
+       <main> 
+
+       </main>
+    </Container>
+    <Footer></Footer>
   </>
   );
 }
